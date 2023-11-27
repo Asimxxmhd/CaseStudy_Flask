248886ec5f4d08958bd62ac4174285e093cddbe2
Asimxxmhd <95497406+Asimxxmhd@users.noreply.github.com>
2023-11-27 16:06:34 +0530

    Update model.py

--- model.py
+++ model.py
@@ -45,5 +45,5 @@ def predict():
         else:
             return render_template('result.html', prediction="Prediction not available")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
